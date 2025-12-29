c388cfc34712473ea963fa6f507369e824f136a7
Thange Sarthak Somnath <sarthakthange1904@gmail.com>
2025-12-29 13:58:30 +0530

    Implement async search in doubly linked list
    Added asynchronous search functions to traverse the doubly linked list in both directions.

--- stroustrup's_ppp/ch_15/doubly_linked_list.cpp
+++ stroustrup's_ppp/ch_15/doubly_linked_list.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <future>
+#include <chrono>
 using namespace std;
 
 class Node{
@@ -51,7 +53,60 @@ void delete_Node(Node *node){
     delete node;
 }
 
+void go_right(Node* start, string str){
+    Node*p = start;
+    int i = 0;
+    while (p != nullptr)
+    {
+        if (p->name == str)
+        {
+            cout << i << " steps right " << " " << p << " " << p->name << endl;
+            return ;
+        }
+        else{
+            p = p->succ;
+            i++;
+        }
+    }
+}
+
+void go_left(Node* start, string str){
+    Node*p = start;
+    int i = 0;
+    while (p != nullptr)
+    {
+        if (p->name == str)
+        {
+            cout << i << " steps left " << " " << p << " " << p->name << endl;
+            return ;
+        }
+        else{
+            p = p->prev;
+            i++;
+        }
+    }
+}
+
+void find(Node* start, string str){
+    auto f1 = async(launch::async, go_left, start, str);
+    auto f2 = async(launch::async, go_right, start, str);
+}
+
 int main(){
-    
+    Node *start = create_Node("0");
+    Node *temp_1 = start;
+    for (int i = 1; i < 500000; i++)
+    {
+        temp_1 = temp_1->add(to_string(i));
+    }
+    Node *mid = temp_1;
+    for (int i = 500000; i <= 1000000; i++)
+    {
+        temp_1 = temp_1->add(to_string(i));
+    }
+    auto begin = chrono::high_resolution_clock::now();
+    find(mid, "999999");
+    auto end = chrono::high_resolution_clock::now();
+    cout << (chrono::duration_cast<chrono::microseconds>(end-begin)).count() << " mics" << endl;
     return 0;
 }
