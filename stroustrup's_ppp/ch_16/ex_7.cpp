25de7a8b92ca85d7e7e4c8ca129d4848aec4e610
Thange Sarthak Somnath <sarthakthange1904@gmail.com>
2026-01-01 05:37:59 +0530

    Add function to concatenate strings with a dot

---
+++ stroustrup's_ppp/ch_16/ex_7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string cat_dot(const char *a, const char *b){
+    string str = "";
+    for (int i = 0; a[i] != '\0'; i++)
+    {
+        str += a[i];
+    }
+    str += '.';
+    for (int i = 0; b[i] != '\0'; i++)
+    {
+        str += b[i];
+    }
+    return str;
+}
+
+int main(){
+    const char *first = "Sarthak", *last = "Thange";
+    cout << cat_dot(first, last) << endl;
+    return 0;
+}
